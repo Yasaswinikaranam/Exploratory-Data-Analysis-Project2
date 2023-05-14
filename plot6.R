45f9e28e630d06932b71d4de4891defc196a1b2a
Yasaswinikaranam <132582853+Yasaswinikaranam@users.noreply.github.com>
2023-05-14 23:31:31 +0530

    Exploratory Data Analysis project2

---
+++ plot6.R
@@ -0,0 +1,15 @@
+library(dplyr)
+library(ggplot2)
+ssc_file <- "Source_Classification_Code.rds"
+balti_la_year <- motor_emi %>% filter(fips == "24510" | fips == "06037") %>% 
+  group_by(fips, year) %>% summarise(total = sum(Emissions))
+balti_la_year <- mutate(balti_la_year, 
+                        Unit = ifelse(fips == "24510", "Baltimore City", 
+                                      ifelse(fips == "06037", "Los Angeles County")))
+ggplot(balti_la_year, aes(factor(year), total, 
+                          fill = Unit, label = round(total))) + 
+  geom_bar(stat = "identity") + facet_grid(. ~ Unit) + 
+  ggtitle("Total Motor Vehicle Emissions") +
+  xlab("Year") + ylab("Pm2.5 Emissions in Tons") +
+  theme(plot.title = element_text(hjust = 0.5)) + ylim(c(0, 8000)) +
+  theme_classic() + geom_text(size = 4, vjust = -1)
